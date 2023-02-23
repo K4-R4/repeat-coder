ba2bdb6df18aa306bdf2dee0cee8fa6c47995261
K4-R4 <mabdman512@gmail.com>
2023-02-23 11:52:11 +0900

    feat/issaved: 保存ボタンの変化

--- src/js/content-script.js
+++ src/js/content-script.js
@@ -4,7 +4,7 @@ const target = document.querySelector("#contest-nav-tabs ul");
 // 挿入する要素
 const html =
     `<li id="save-btn"><a href="javascript:void(0)">
-        <span class="glyphicon glyphicon-bookmark" aria-hidden="true"></span>保存
+        <span class="glyphicon glyphicon-save color-not-ok" aria-hidden="true"></span>保存
     </a></li>`;
 target.insertAdjacentHTML("beforeend", html);
 
@@ -19,9 +19,9 @@ target.insertAdjacentHTML("beforeend", html);
         }
     }
 } */
-const sabeBtn = document.getElementById("save-btn");
+const saveBtn = document.getElementById("save-btn");
 // chrome.storage.sync.clear();
-sabeBtn.addEventListener("click", async () => {
+saveBtn.addEventListener("click", async () => {
     const detail = getProblemDetail();
     const problem = {
         title: detail["title"],
@@ -31,16 +31,31 @@ sabeBtn.addEventListener("click", async () => {
 
     // コンテスト単位で問題を保存
     const item = await chrome.storage.sync.get(detail["contest"]);
-    if (item[detail["contest"]] == null) {
+    if(item[detail["contest"]] == null) {
         // 1.コンテスト自体が保存されていない場合は新たに作成
         chrome.storage.sync.set({ [detail["contest"]]: { [detail["url"]]: problem } });
-    } else {
+    }else if(item[detail["contest"]][detail["url"]] == null) {
         // 2.コンテストがすでに保存されているなら追記
         item[detail["contest"]][detail["url"]] = problem;
         await chrome.storage.sync.set(item);
+    }else {
+        // 3.すでに問題が保存されているならそれを削除する
+        const problemsInContest = Object.keys(item[detail["contest"]]).length;
+        if (problemsInContest <= 1) {
+            // コンテストが削除対象の問題だけ含んでいるならコンテスト自体を削除
+            await chrome.storage.sync.remove(detail["contest"]);
+        } else {
+            // 問題を削除
+            delete item[detail["contest"]][detail["url"]];
+            await chrome.storage.sync.set(item);
+        }
     }
-    console.log(item);
+
+    toggleButtonColor();
 });
+
+
+init();
 
 
 function getProblemDetail() {
@@ -54,5 +69,27 @@ function getProblemDetail() {
     return { url: url, contest: contest, title: title, savedAt: savedAt };
 }
 
-// todo 保存タブを押したあとのビジュアル変化
-// todo 問題のランダムな提示
+
+// ページの問題がすでに保存されているか否かを判定
+async function isSaved() {
+    const detail = getProblemDetail();
+    const item = await chrome.storage.sync.get(detail["contest"]);
+
+    if(item[detail["contest"]] == null || item[detail["contest"]][detail["url"]] == null) return false;
+    return true;
+}
+
+function toggleButtonColor() {
+    const icon = saveBtn.querySelector("span");
+    icon.classList.toggle("glyphicon-save");
+    icon.classList.toggle("glyphicon-saved");
+    icon.classList.toggle("color-ok");
+    icon.classList.toggle("color-not-ok");
+}
+
+async function init() {
+    // ページを読み込んだ時点で問題が保存されているなら保存ボタンを切り替え
+    if(await isSaved()) {
+        toggleButtonColor();
+    }
+}
